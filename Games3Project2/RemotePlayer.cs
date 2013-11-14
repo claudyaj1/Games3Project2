848e9bc16353925b4f1dd3ce64275c39b0b5ab54
Cole Stoltzfus <stoltzfuscj1@gcc.edu>
2013-11-14 10:08:48 -0500

    Adding RemotePlayer.cs file

---
+++ Games3Project2/RemotePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+
+using Games3Project2.Globals;
+using Broad.Camera;
+
+
+namespace Games3Project2
+{
+    class RemotePlayer : Collidable
+    {
+        public RemotePlayer(Game game, Vector3 pos) :
+            base(game, pos, Vector3.Zero, Global.Constants.PLAYER_RADIUS)
+        {
+
+        }
+    }
+}
