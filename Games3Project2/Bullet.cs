a38bc0044ea2486f8258eb85a63579c112a98978
Andrew Claudy <andrew.claudy@live.com>
2013-11-20 03:12:16 -0500

    Adds a bullet bounding sphere renderer, untested.

---
+++ Games3Project2/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+using BoundingVolumeRendering;
+using Games3Project2.Globals;
+
+namespace Games3Project2
+{
+    public class Bullet : Collidable
+    {
+         BoundingSphere bs;
+
+        public Bullet(Vector3 startPosition, Vector3 velocity) :
+            base(Global.game, startPosition, velocity, Global.Constants.BULLET_RADIUS)
+        {
+            bs = new BoundingSphere(startPosition, Global.Constants.BULLET_RADIUS);
+        }
+
+        public void update(GameTime gametime)
+        {
+            position = position + velocity * gametime.ElapsedGameTime.Milliseconds;
+            bs.Center = position;
+        }
+
+        public void draw()
+        {
+            BoundingSphereRenderer.Draw(bs,
+                Global.CurrentCamera.view,
+                Global.CurrentCamera.projection);
+        }
+            
+    }
+}
