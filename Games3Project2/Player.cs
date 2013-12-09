9481f98e0235cfd90327633971e5520053917172
Cole Stoltzfus <stoltzfuscj1@gcc.edu>
2013-12-09 01:35:25 -0500

    More interval fun

--- Games3Project2/Player.cs
+++ Games3Project2/Player.cs
@@ -38,7 +38,7 @@ namespace Games3Project2
         public LocalNetworkGamer gamer;
 
         int timeSinceLastPacketSent = 0;
-        const int PACKET_INTERVAL = 10;
+        const int PACKET_INTERVAL = 600;
 
         public override Vector3 Position
         {
